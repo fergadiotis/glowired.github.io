04a2d4ffacfcfa15335923215e8afc7fee9ab55f
boubex <bitiabt@gmail.com>
2016-09-04 19:07:21 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,5 @@
+//all images by unsplash.com
+//Chicago by Blaise Sewell: https://thenounproject.com/term/chicago-skyline/76262/
+$('.btn').prepend('<div class="hover"><span></span><span></span><span></span><span></span><span></span></div>');
+
+$('.social-btn').prepend('<div class="hover"><span></span><span></span><span></span><span></span></div>');
